f9ee2c184dbbfb074309f6f4964ef3729f271415
william-francia <wfranciapoma@byupathway.edu>
2025-09-25 00:49:51 -0400

    changes

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,3 +1,10 @@
+const currentYear = new Date().getFullYear();
+
+document.getElementById("currentyear").textContent = currentYear;
+
+const lastModified = document.lastModified;
+
+document.getElementById("lastModified").textContent = `Last Modified: ${lastModified}`;
 
 
 const nav = document.querySelector ("#nav")
